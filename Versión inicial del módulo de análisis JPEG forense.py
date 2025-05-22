0628b9d4df1218bd2dc6eab11f0f8cee73f08c73
Monica <polonio87@gmail.com>
2025-05-22 16:15:01 +0200

    Create Versión inicial del módulo de análisis JPEG forense.py

---
+++ Versión inicial del módulo de análisis JPEG forense.py
@@ -0,0 +1,21 @@
+#Versión inicial del módulo de análisis JPEG forense
+
+#Descargar e instalar exiftool en tu ordenador
+#https://exiftool.org/
+
+#Descarga metadatos de una imagen jpg
+exiftool imagen.jpeg
+
+#Descarga tablas de cuantificación en valores hexadecimales
+exiftool -v3 imagen.jpg
+
+
+#Descarga metadatos de imagen jpg ordenados por categorias
+exiftool -a -u -g1 + imagen.jpg 
+
+#Descarga metadatos de imagen jpg ordenados por categorias
+exiftool -a -u -g1 + imagen.jpg 
+
+
+
+
